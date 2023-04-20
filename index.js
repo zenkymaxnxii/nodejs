7ff843f54900727e1bd63af050f185ef1afe829c
zenkymaxnxii <quanhh.nang@gmail.com>
2023-04-20 15:22:56 +0700

    add

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const PORT = 3000;
+const ChatTokenBuilder = require("./src/ChatTokenBuilder").ChatTokenBuilder;
+const appId = "366d251855ce47eb8d387301075c279b";
+const appCertificate = "e0514bcd56ed4c43842e9e1dd9b73d93";
+const expirationInSeconds = 60*60*24;
+
+const appToken = ChatTokenBuilder.buildAppToken(appId, appCertificate, expirationInSeconds)
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.end(appToken);
 });
 
 server.listen(PORT, () => {
